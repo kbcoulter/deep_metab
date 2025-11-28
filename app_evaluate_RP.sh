c9eccc7e47f0a76f55af9bd5d60e8fc2dcf0d92f
kbcoulter <coulterkb@yahoo.com>
2025-11-28 13:42:29 -0800

    Created new setup scripts instead of a list of commands to run, edited evaluate to show changes made clearly to raise issue on GRT repo later, and added support for HILIC model evaluation, in submodule branch of same name

--- app_evaluate_RP.sh
+++ app_evaluate_RP.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-### NOTE: RUN THIS FROM deep_metab
+### NOTE: RUN THIS FROM deep_metab DIR
 
 #SBATCH --account=bgmp
 #SBATCH --partition=gpu
-#SBATCH --job-name=deep_metab_apptainer_eval
+#SBATCH --job-name=apptainer_eval_RP
 #SBATCH --time=3:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -12,30 +12,23 @@
 #SBATCH --output=evaluate_RP_%j.out.log
 #SBATCH --error=evaluate_RP_%j.err.log
 
-# Define a folder name for all Graphormer-RT predictions you intend on producing
-PREDS_DIR="predictions_RP"
-mkdir -p "$PREDS_DIR"
+HOST_DATA_DIR="/projects/bgmp/shared/groups/2025/deepmetab/kcoulter/deep_metab/sample_data_0001"
+# SET ABSOLUTE PATH CONTAINING 1 .csv FOR RT AND 1 .pkl FOR CHROMATOGRAPHY CONDITIONS
 
-# Data file of this Graphormer-RT prediction instance, initialized to <sbatch job #>.csv
+#PREDS_DIR="predictions_RP"
+#mkdir -p "$PREDS_DIR"
+
 DATA_DIR="${SLURM_JOB_ID}.csv"
-
-# Define the HOST directory containing your data.
-# NOTE: There are 2 necessary specifications. 1) The directory is an absolute path. 2) The directory only contains 2 files, a .csv for retention time data and a .pickle for chromatography sample conditions.
-HOST_DATA_DIR="/projects/bgmp/shared/groups/2025/deepmetab/ewi/deep_metab/sample_data_0001"
-#HOST_DATA_DIR="/projects/bgmp/shared/groups/2025/deepmetab/ewi/deep_metab/sample_data_from_graphormer"
 CONTAINER_DATA_FILE=$(find "$HOST_DATA_DIR" -maxdepth 1 -name "*.csv" -print -quit) # <-- Retention time data
 CONTAINER_METADATA_FILE=$(find "$HOST_DATA_DIR" -maxdepth 1 -name "*.pickle" -print -quit) # <-- Chromatography sample conditions
 
-# Run evaluate.py in Graphormer-RT under our container
 apptainer exec --nv \
     --bind ./graphormer_checkpoints_RP:/workspace/Graphormer-RT/checkpoints_RP \
     --bind ${HOST_DATA_DIR}:/data \
     graphormercontainer.sif bash -c "
     source /opt/conda/bin/activate /opt/conda/envs/graphormer-rt && \
-    
     export RP_DATA_FILE_PATH=\"${CONTAINER_DATA_FILE}\"
     export RP_METADATA_PATH=\"${CONTAINER_METADATA_FILE}\"
-
     cd ./Graphormer-RT/graphormer/evaluate/ && \
     python evaluate.py \
         --user-dir ../../graphormer \
@@ -54,7 +47,7 @@ apptainer exec --nv \
         --mlp-layers 5 \
         --batch-size 64 \
         --num-classes 1 \
-        --save-path '../../../$PREDS_DIR/$DATA_DIR' \
+        --save-path '../../../predictions_RP/$DATA_DIR' \
         --save-dir '/workspace/Graphormer-RT/checkpoints_RP/' \
         --split train
 "
