78aa2c1a617a4a8200a110d112f4c97d75255bde
kbcoulter <coulterkb@yahoo.com>
2025-12-03 18:10:46 -0800

    Changes to HILIC_test.py to support HILIC evaluation, new script to create HILIC knowns dataset input for testing, typo fix in app_evaluate_HILIC.sh and setup HILIC_RP.sh script

--- setup_HPC/HILIC_RP.sh
+++ setup_HPC/HILIC_RP.sh
@@ -14,7 +14,7 @@ echo "HILIC Setup Complete. Beginning RP."
 
 mkdir -p graphormer_checkpoints_RP
 wget -O graphormer_checkpoints_RP/oct30_RP_unc.pt https://zenodo.org/records/15021743/files/oct30_RP_unc.pt?download=1
-mkdir -p predictions_HILIC
+mkdir -p predictions_RP
 echo "RP Setup Complete."
 
 echo "Setup Completed Successfully!"
