a3a76d1bf6760ae8d74b3c5c20f4ada97d47b38e
kbcoulter <coulterkb@yahoo.com>
2025-12-30 13:30:24 -0800

    Cleaning Repo and Adding Support (Option 1) for data prep. Scripts I am unsure of are left unfiled in their parent dir. I deleted duplicates but left most of the prep scripts unchanged.

--- data_prep/initial_clean_RT.py
+++ data_prep/initial_clean_RT.py
@@ -8,7 +8,6 @@ from pathlib import Path
 #INPUTS AND OUTPUTS#
 ####################
 
-
 parser = argparse.ArgumentParser(
     description="Process a file and save the output in a TSV."
 )
@@ -16,8 +15,9 @@ parser = argparse.ArgumentParser(
 parser.add_argument(
     "--option",
     type=int,  
-    required=True,
+    required=False,
     help="Options for format of output files: 1) simple format for Graphormer-RT processing, or 2) complicated format for reference.",
+    default = 1
 )
 
 parser.add_argument(
